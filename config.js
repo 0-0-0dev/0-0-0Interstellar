5806adfb4c201f4f625a9a643e1303cbfadddbff
0-0-0dev <tyler.pascual169@schools.sa.edu.au>
2026-09-05 08:44:37 +0000

    port 9999 strict

--- config.js
+++ config.js
@@ -2,8 +2,11 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "67",
+    tyler: "2112",
+    
   },
 };
 
 export default config;
+
+const PORT = 9999;
