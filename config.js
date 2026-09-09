25f3d9dccce26bb5dfa3c9ae8f0de24c1f4322c1
0-0-0dev <tyler.pascual169@schools.sa.edu.au>
2026-09-09 00:26:45 +0000

    passwords

--- config.js
+++ config.js
@@ -3,10 +3,9 @@ const config = {
   users: {
     // You can add multiple users by doing username: 'password'.
     tyler: "2112",
-    
+    arbin: "5046",
+    alex: "hotdog67",
   },
 };
 
 export default config;
-
-const PORT = 9999;
